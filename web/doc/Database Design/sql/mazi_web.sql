9ff5e7d2ea50cee20c9a6f202c8ef1413233479d
Allen Galler <allengaller@gmail.com>
2013-11-18 21:13:00 +0800

    update
    update

---
+++ web/doc/Database Design/sql/mazi_web.sql
@@ -0,0 +1,198 @@
+NO_DIR_IN_CREATE DATABASE  IF NOT EXISTS `mysql`
+USE `mysql`;
+
+DROP TABLE IF EXISTS `accounts_admin`;
+CREATE TABLE `accounts_admin` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`group_id` int(11) DEFAULT NULL COMMENT 'Group ID',
+	`account` varchar(45) DEFAULT NULL COMMNET 'Account Name',
+	`password` varchar(64) DEFAULT NULL COMMENT 'Password',
+	`role` tinytext COMMENT 'Each role have one permission sets.',
+	`name` varchar(45) DEFAULT '' COMMENT 'Name',
+	`gender` tinyint(4) DEFAULT '1' COMMENT '1:Male,2:Female',
+	`mobile` bigint(11) DEFAULT NULL COMMNET 'Mobile Phone',
+	`email` varchar(64) DEFAULT '',
+	`remark` varchar(125) DEFAULT '',
+	`status` tinyint(4) DEFAULT NULL COMMNET '1:Normal,2:Disable,3:Delete.',
+	`created` datetime DEFAULT NULL,
+	`modified` datetime DEFAULT NULL,
+	PRIMARY KEY ('id')
+	UNIQUE KEY `email` (`email`)
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='Admin Account'
+
+DROP TABLE IF EXISTS `accounts_user`;
+CREATE TABLE `accounts_user` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`group_id` int(11) DEFAULT NULL COMMENT 'Group ID',
+	`account` varchar(45) DEFAULT NULL COMMNET 'Account Name',
+	`password` varchar(64) DEFAULT NULL COMMENT 'Password',
+	`role` tinytext COMMENT 'Each role have one permission sets.',
+	`name` varchar(45) DEFAULT '' COMMENT 'Name',
+	`first_name` varchar(64) NOT NULL,
+  	`last_name` varchar(64) NOT NULL,
+	`gender` tinyint(4) DEFAULT '1' COMMENT '1:Male,2:Female',
+	`mobile` bigint(11) DEFAULT NULL COMMNET 'Mobile Phone',
+	`email` varchar(64) DEFAULT '',
+	`remark` varchar(125) DEFAULT '',
+	`follower_id` int(11) NOT NULL,
+	`followee_id` int(11) NOT NULL,
+	`follow_time` int(11) NOT NULL,
+	`status` int(11) NOT NULL COMMNET '1:Normal,2:Disable,3:Delete.',
+	`country` int(32) NOT NULL,
+	`province` int(32) NOT NULL,
+	`city` int(32) NOT NULL,
+	`county` int(32) NOT NULL,
+	`street` varchar(32) DEFAULT NULL,
+	`created` datetime DEFAULT NULL,
+	`modified` datetime DEFAULT NULL,
+	KEY `follower_id` (`follower_id`, `followee_id`),
+	KEY `followee_id` (`followee_id`)
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8_general_ci COMMENT='User Account'
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`uid` int(11) NOT NULL,
+	`pid` int(11) DEFAULT NULL,
+	`content` varchar(256) NOT NULL,
+	`abstract` varchar(128) DEFAULT NULL,
+	`post_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8_general_ci COMMENT='Posts from users.'
+
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE `books` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8_general_ci COMMENT='Books from users.'
+
+DROP TABLE IF EXISTS `memos`;
+CREATE TABLE `memos` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8_general_ci COMMENT='Memos from users.'
+
+
+DROP TABLE IF EXISTS `journals`;
+CREATE TABLE `journals` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8_general_ci COMMENT='Journals from users.'
+
+DROP TABLE IF EXISTS `carts`;
+CREATE TABLE `carts` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`session_id` int(11) NOT NULL,
+	`uid` int(11) DEFAULT NULL,
+	`pid` int(11) NOT NULL,
+	`pcates` int(11) NOT NULL,
+	`units` int(11) NOT NULL,
+	`carts_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	KEY `session_id` (`session_id`,`user_id`,`product_id`),
+  	KEY `user_id` (`user_id`),
+  	KEY `product_id` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8_general_ci COMMENT='Carts from users.'
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`session_id` int(11) NOT NULL,
+	`uid` int(11) DEFAULT NULL,
+	`pid` int(11) NOT NULL,
+	`pcates` int(11) NOT NULL,
+	`units` int(11) NOT NULL,
+	`status` tinyint(4) DEFAULT NULL COMMENT ''
+	`carts_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	KEY `session_id` (`session_id`,`user_id`,`product_id`),
+  	KEY `user_id` (`user_id`),
+  	KEY `product_id` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8_general_ci COMMENT='Orders from user to user.'
+
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE `tags` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8_general_ci COMMENT='Tags from users.'
+
+DROP TABLE IF EXISTS `cates`;
+CREATE TABLE `cates` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT, `name` VARCHAR(255) NOT NULL,
+	`slug` VARCHAR(255) NOT NULL, `parent_id` INT(11) DEFAULT NULL,
+	`lft` INT(11) DEFAULT NULL,
+	`rght` INT(11) DEFAULT NULL,
+	`link_count` INT(11) NOT NULL DEFAULT'0', 
+	PRIMARY KEY (`id`)
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='Categories';
+
+DROP TABLE IF EXISTS `posts_tags`;
+CREATE TABLE `posts_tags` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='posts_tags';
+
+DROP TABLE IF EXISTS `posts_cates`;
+
+
+DROP TABLE IF EXISTS `pages`;
+CREATE TABLE `pages` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='pages';
+
+DROP TABLE IF EXISTS `pages_tags`;
+CREATE TABLE `pages_tags` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='pages_tags';
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='roles';
+
+DROP TABLE IF EXISTS `permissions`;
+CREATE TABLE `permissions` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='permissions';
+
+DROP TABLE IF EXISTS `media`;
+CREATE TABLE `media` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='media';
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='comments';
+
+DROP TABLE IF EXISTS `themes`;
+CREATE TABLE `themes` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='themes';
+
+DROP TABLE IF EXISTS `tools`;
+CREATE TABLE `tools` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='tools';
+
+DROP TABLE IF EXISTS `plugins`;
+CREATE TABLE `plugins` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='plugins';
+
+DROP TABLE IF EXISTS `settings`;
+CREATE TABLE `settings` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`),
+) ENGINE=MyISM DEFAULT CHARSET=utf8_general_ci COMMNET='settings';
