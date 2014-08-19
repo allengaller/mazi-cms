4ae1175ca8bb7f662175805c47fc80afd99d7b16
Allen Galler <allengaller@gmail.com>
2014-08-19 16:53:12 +0800

    update
    update

--- web/public_html/app/Model/Permission.php
+++
@@ -1,9 +0,0 @@
-<?php
-App::uses('AppModel', 'Model');
-/**
- * Permission Model
- *
- */
-class Permission extends AppModel {
-
-}
--- web/public_html/app/Model/Tool.php
+++
@@ -1,9 +0,0 @@
-<?php
-App::uses('AppModel', 'Model');
-/**
- * Tool Model
- *
- */
-class Tool extends AppModel {
-
-}
